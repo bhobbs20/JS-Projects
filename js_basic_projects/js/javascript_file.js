a249820bdf2efd6a2a2b6802650d27c3408db2d2
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-04 10:43:11 -0500

    js lesson

---
+++ js_basic_projects/js/javascript_file.js
@@ -0,0 +1,23 @@
+// var x = "Let's get wild Lets get wild!" + " Lets go dancing";
+// window.alert(x);
+
+// var Family = "The Arezzinis", Dad = "Jermiah", Mom ="Hermoine",
+//     Daughter = "Penny", Son = "Zorro";
+// document.write(Dad);
+
+// var blues = "I have the blues";
+// var blues = blues.fontcolor("blue");
+// document.write(blues);
+
+// var blue = "blue";
+// var blue = blue.fontcolor("blue");
+// document.write(blue);
+
+// document.write(3 + 3 *9);
+
+function myFirstFunction() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
+
