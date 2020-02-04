41ec28a964dc709803965dafa1e16717f568fd11
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-04 12:00:34 -0500

    add js and html files and add a button function

---
+++ js_basic_projects/js/basic_javascript_2.js
@@ -0,0 +1,7 @@
+
+function displayText() {
+    var text = "Hey, whats up guys, wanna play cards?";
+    var result = text.fontcolor("blue");
+    document.getElementById("text").innerHTML = result;
+}
+
