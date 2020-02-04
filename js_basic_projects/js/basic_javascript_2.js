f99eab52f4e531169d61c9bf106f25870de5839b
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-04 13:22:31 -0500

    add concatenated string

--- js_basic_projects/js/basic_javascript_2.js
+++ js_basic_projects/js/basic_javascript_2.js
@@ -1,6 +1,7 @@
 
 function displayText() {
     var text = "Hey, whats up guys, wanna play cards?";
+    text += "or you wanna study some code?"
     var result = text.fontcolor("blue");
     document.getElementById("text").innerHTML = result;
 }
