4d2729aa226bb5267c5a0ca907b2d573840ce1ec
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-04 13:55:07 -0500

    add basic math functions

---
+++ js_basic_projects/js/basic_javascript_3.js
@@ -0,0 +1,19 @@
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("math").innerHTML = "2 + 2 = " + addition;
+}
+
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("math").innerHTML = "5 - 2 = " + subtraction; 
+}
+
+function multiplication() {
+    var simple_math = 6 * 8;
+    document.getElementById("math").innerHTML = "6 * 8 = " + simple_math;
+}
+
+function division() {
+    var simple_math = 48 / 6;
+    document.getElementById("math").innerHTML = "48 / 6 = " + simple_math;
+}
