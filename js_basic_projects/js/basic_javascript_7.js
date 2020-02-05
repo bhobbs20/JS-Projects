81e693b89247b2c9abb89b2ef7d666821be8b4a0
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-05 12:51:01 -0500

    added date() function

--- js_basic_projects/js/basic_javascript_7.js
+++ js_basic_projects/js/basic_javascript_7.js
@@ -9,15 +9,36 @@
 // Add_Numbers_1();
 // Add_Numbers_2();
 
-var number = 28;
+// var number = 28;
 
-function divideNum() {
-    var number = 10;
-    console.log(number / 5);
-}
-function mulitplyNum() {
-    console.log(new_number * 100);
+// function divideNum() {
+//     var number = 10;
+//     console.log(number / 5);
+// }
+// function mulitplyNum() {
+//     console.log(new_number * 100);
+// }
+
+// divideNum();
+// mulitplyNum();
+
+// if (1 < 2) {
+//     document.write("the left number is smaller that the number on the right")
+// }
+
+function myTime() {
+    my_time = new Date().getHours();
+    what_to_eat = (my_time < 12) ? "Steak and Eggs" : "Burger and Beer";
+    document.getElementById("time").innerHTML = what_to_eat;
 }
 
-divideNum();
-mulitplyNum();
+function get_Date() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "How are you Today";
+    }
+}
+
+var number = 24;
+if (number < 32) {
+    document.write("Whats up");
+}
