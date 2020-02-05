90adc1fb76304fcac7534ce638da21b31d5ca476
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-05 12:18:40 -0500

    added global var and function with err

---
+++ js_basic_projects/js/basic_javascript_7.js
@@ -0,0 +1,23 @@
+// var x = 10;
+// function Add_Numbers_1() {
+//     document.write(20 + x + "<br>");
+// }
+// function Add_Numbers_2() {
+//     document.write(x + 100);
+// }
+
+// Add_Numbers_1();
+// Add_Numbers_2();
+
+var number = 28;
+
+function divideNum() {
+    var number = 10;
+    console.log(number / 5);
+}
+function mulitplyNum() {
+    console.log(new_number * 100);
+}
+
+divideNum();
+mulitplyNum();
