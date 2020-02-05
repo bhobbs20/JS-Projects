a32c529d32288c7eef05e388e4b0fa19a863733b
Brian Hobbs <brianhobbs216@gmail.com>
2020-02-05 06:51:07 -0500

    add '&&' and '||' operators

--- js_basic_projects/js/basic_javascript_5.js
+++ js_basic_projects/js/basic_javascript_5.js
@@ -1,6 +1,6 @@
-document.write(typeof "word");
+// document.write(typeof "word");
 
-document.write(typeof 3);
+// document.write(typeof 3);
 
 function my_function() {
     // document.getElementById("test").innerHTML = 0/0;
@@ -8,33 +8,41 @@ function my_function() {
     document.getElementById("test").innerHTML = isNaN('007');
 }
 
-document.write(2E310);
-document.write(-3E310);
+// document.write(2E310);
+// document.write(-3E310);
 
-document.write(10 < 2);
-document.write(100 > 87);
+// document.write(10 < 2);
+// document.write(100 > 87);
 
-console.log(10 * 42)
+// console.log(10 * 42)
 
-document.write("10" + 5);
+// document.write("10" + 5);
 
-console.log( 10 > 5);
+// console.log( 10 > 5);
 
-document.write(10 == 10);
-document.write(3 == 11);
+// document.write(10 == 10);
+// document.write(3 == 11);
 
-x = 10;
-y = 10;
-document.write(x === y);
+// x = 10;
+// y = 10;
+// document.write(x === y);
 
-x = 82;
-y = "82";
-document.write(x === y);
+// x = 82;
+// y = "82";
+// document.write(x === y);
 
-a = "magnus";
-b = "magnus";
-document.write(a === b);
+// a = "magnus";
+// b = "magnus";
+// document.write(a === b);
 
-t = 78;
-h = 67;
-document.write(t === h);
+// t = 78;
+// h = 67;
+// document.write(t === h);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write( 5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
